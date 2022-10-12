7571531ec6ad7c6225eea3f2706fac399b85e4ca
nathan-gissler <nathangissler@gmail.com>
2022-10-12 16:23:58 +0200

    add symbols in score, note deletion, update readme

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# mon-sheet-cest-moi
+# Mon sheet c'est moi
+Site web permettant d'écrire de la musique sur une partition intéractive.
+
+## Edition de la partition
+Sur la page `Ma partition` :
+- changer de mode d'édition en appuyant sur `a` (add note) ou `d` (delete note)
+- en mode ajout, cliquer sur la portée pour ajouter des notes
+- en mode suppression, cliquer sur les notes pour les supprimer
--- score.js
+++ score.js
@@ -2,11 +2,12 @@ const canvas = document.getElementById('score');
 const ctx = canvas.getContext('2d');
 const staffLineSpacing = 20;
 const symbolSpacing = 20;
-const imgRatio = {"whole_note": 320/176, "treble_clef": 87/238};
+const imgRatio = {"whole_note": 320/176, "treble_clef": 87/238, "time_signature_4": 348/480, "time_signature_4_4": 134/329, "whole_rest": 232/479};
 
 ctx.fillStyle = 'white';
 ctx.fillRect(0, 0, 800, 500);
 
+/* pour créer une ligne avec des coordonnées */
 class Line {
     constructor(x1, y1, x2, y2) {
         this.x1 = x1;
@@ -23,50 +24,7 @@ class Line {
     }
 }
 
-class Staff {
-    constructor(xpos, ypos, length, lineSpacing = staffLineSpacing) {
-        this.xpos = xpos;
-        this.ypos = ypos;
-        this.length = length;
-        this.lineSpacing = lineSpacing;
-        this.lines = [];
-        this.symbols = [];
-        this.horPlaceForNextSymbol = 0;
-    }
-
-    draw(ctx) {
-        for (let i = 0; i < 5; i++) {
-            let line = new Line(this.xpos, this.ypos + i * this.lineSpacing, this.xpos + this.length, this.ypos + i * this.lineSpacing);
-            line.draw(ctx);
-            this.lines.push(line);
-        }
-        this.addClef("treble_clef");
-    }
-
-    addNote(noteFigure, noteValue) {
-        let note = new Note(noteFigure, noteValue, this, this.horPlaceForNextSymbol);
-        note.draw(ctx);
-        this.symbols.push(note);
-        this.horPlaceForNextSymbol += noteFigure.width + symbolSpacing;
-    }
-
-    addClef(name) {
-        let clef = new Clef(name, this, this.horPlaceForNextSymbol);
-        clef.draw(ctx);
-        this.symbols.push(clef);
-        this.horPlaceForNextSymbol += clef.width + symbolSpacing;
-    }
-}
-
-class NoteFigure {
-    constructor(name) {
-        this.name = name;
-        this.height = staffLineSpacing;
-        this.width = this.height * imgRatio[name];
-        this.imagePath = "img/" + name + ".png";
-    }
-}
-
+/* pour créer une image avec position et dimensions */
 class CanvasImage {
     constructor(path, xpos, ypos, width, height) {
         this.path = path;
@@ -89,6 +47,84 @@ class CanvasImage {
     }
 }
 
+/* symbole de clef avec ses coordonnées et dimensions qui sera placé sur une portée */
+class Clef {
+    constructor(name, staff, horPlaceInStaff) {
+        this.name = name;
+        this.staff = staff;
+        this.setHorPlaceInStaff(horPlaceInStaff);
+        this.ypos = this.staff.ypos - 27;
+        this.height = 140;
+        this.width = this.height * imgRatio[name];
+        this.imagePath = "img/" + name + ".png";
+    }
+
+    setHorPlaceInStaff(x) {
+        this.horPlaceInStaff = x;
+        this.xpos = this.staff.xpos + x;
+    }
+
+    draw(ctx) {
+        let image = new CanvasImage(this.imagePath, this.xpos, this.ypos, this.width, this.height);
+        image.draw(ctx);
+    }
+}
+
+/* indication de mesure */
+class TimeSignature {
+    constructor(name, staff, horPlaceInStaff) {
+        this.name = name;
+        this.staff = staff;
+        this.setHorPlaceInStaff(horPlaceInStaff);
+        this.ypos = this.staff.ypos;
+        this.height = staffLineSpacing * 4;
+        this.width = this.height * imgRatio[name];
+        this.imagePath = "img/" + name + ".png";
+    }
+
+    setHorPlaceInStaff(x) {
+        this.horPlaceInStaff = x;
+        this.xpos = this.staff.xpos + x;
+    }
+
+    draw(ctx) {
+        let image = new CanvasImage(this.imagePath, this.xpos, this.ypos, this.width, this.height);
+        image.draw(ctx);
+    }
+}
+
+/* barre de mesure */
+class BarLine {
+    constructor(staff, horPlaceInStaff) {
+        this.staff = staff;
+        this.setHorPlaceInStaff(horPlaceInStaff);
+        this.ypos = this.staff.ypos;
+        this.height = staffLineSpacing * 4;
+        this.width = 0;
+    }
+
+    setHorPlaceInStaff(x) {
+        this.horPlaceInStaff = x;
+        this.xpos = this.staff.xpos + x;
+    }
+
+    draw(ctx) {
+        let line = new Line(this.xpos, this.ypos, this.xpos, this.ypos + this.height);
+        line.draw(ctx);
+    }
+}
+
+/* figure de note, correspondant à la durée de la note (noire, blanche, ronde...) */
+class NoteFigure {
+    constructor(name) {
+        this.name = name;
+        this.height = staffLineSpacing;
+        this.width = this.height * imgRatio[name];
+        this.imagePath = "img/" + name + ".png";
+    }
+}
+
+/* hauteur de note (la4, ré#3...) avec la fréquence et la hauteur sur la portée correspondantes */
 class NoteValue {
     constructor(name, octave, alteration, frequency, vertPlaceInStaff) {
         this.name = name;
@@ -99,15 +135,22 @@ class NoteValue {
     }
 }
 
+/* une note qui pourra être placée sur la portée, on lui attribue une figure de note (durée) et une hauteur */
 class Note {
     constructor(noteFigure, noteValue, staff, horPlaceInStaff) {
         this.noteFigure = noteFigure;
         this.noteValue = noteValue;
         this.staff = staff;
-        this.horPlaceInStaff = horPlaceInStaff;
+        this.setHorPlaceInStaff(horPlaceInStaff);
         this.vertPlaceInStaff = noteValue.vertPlaceInStaff;
-        this.xpos = staff.xpos + horPlaceInStaff;
         this.ypos = staff.ypos + this.vertPlaceInStaff * staffLineSpacing / 2;
+        this.height = noteFigure.height;
+        this.width = noteFigure.width;
+    }
+
+    setHorPlaceInStaff(x) {
+        this.horPlaceInStaff = x;
+        this.xpos = this.staff.xpos + x;
     }
 
     draw(ctx) {
@@ -116,29 +159,159 @@ class Note {
     }
 }
 
-class Clef {
-    constructor(name, staff, horPlaceInStaff) {
+/* figure de silence, correspondant à une durée et un symbole spécifiques */
+class RestFigure {
+    constructor(name) {
         this.name = name;
-        this.staff = staff;
-        this.horPlaceInStaff = horPlaceInStaff;
-        this.xpos = staff.xpos + horPlaceInStaff;
-        this.ypos = this.staff.ypos - 27;
-        this.height = 140;
+        this.height = staffLineSpacing * 4;
         this.width = this.height * imgRatio[name];
         this.imagePath = "img/" + name + ".png";
     }
+}
+
+/* un silence qui pourra être placé sur la portée auquel on attribue une figure (durée) */
+class Rest {
+    constructor(restFigure, staff, horPlaceInStaff) {
+        this.restFigure = restFigure;
+        this.staff = staff;
+        this.setHorPlaceInStaff(horPlaceInStaff);
+        this.ypos = staff.ypos;
+        this.height = restFigure.height;
+        this.width = restFigure.width;
+    }
+
+    setHorPlaceInStaff(x) {
+        this.horPlaceInStaff = x;
+        this.xpos = this.staff.xpos + x;
+    }
 
     draw(ctx) {
-        let image = new CanvasImage(this.imagePath, this.xpos, this.ypos, this.width, this.height);
+        let image = new CanvasImage(this.restFigure.imagePath, this.xpos, this.ypos, this.restFigure.width, this.restFigure.height);
         image.draw(ctx);
     }
 }
 
-const staff = new Staff(50, 110, 700);
-staff.draw(ctx);
+/* une portée qui pourra être affichée dans le canvas */
+class Staff {
+    constructor(xpos, ypos, length, lineSpacing = staffLineSpacing) {
+        this.xpos = xpos;
+        this.ypos = ypos;
+        this.length = length;
+        this.lineSpacing = lineSpacing;
+        this.lines = [];
+        /* liste des symboles placés sur la clef */
+        this.symbols = [];
+        /* position du prochain symbole à placer */
+        this.horPlaceForNextSymbol = 0;
+
+        this.draw(ctx);
+
+        /* symboles qui apparaissent sur la portée au départ */
+        this.addClef("treble_clef");
+        this.addTimeSignature("time_signature_4_4");
+        let wholeRest = new RestFigure("whole_rest");
+        this.addRest(wholeRest);
+        this.addBarLine();
+        this.addRest(wholeRest);
+        this.addBarLine();
+    }
+
+    draw(ctx) {
+        /* affichage des 5 lignes */
+        for (let i = 0; i < 5; i++) {
+            let line = new Line(this.xpos, this.ypos + i * this.lineSpacing, this.xpos + this.length, this.ypos + i * this.lineSpacing);
+            line.draw(ctx);
+            this.lines.push(line);
+        }
+
+        /* affichage des symboles de la liste */
+        for (let i = 0; i < this.symbols.length; i++) {
+            this.drawSymbol(this.symbols[i], ctx);
+        }
+    }
+
+    addClef(name) {
+        let clef = new Clef(name, this, this.horPlaceForNextSymbol);
+        this.addSymbol(clef);
+    }
+
+    addTimeSignature(name) {
+        let timeSignature = new TimeSignature(name, this, this.horPlaceForNextSymbol);
+        this.addSymbol(timeSignature);
+    }
+
+    addBarLine() {
+        let barLine = new BarLine(this, this.horPlaceForNextSymbol);
+        this.addSymbol(barLine);
+    }
+
+    addNote(noteFigure, noteValue) {
+        let note = new Note(noteFigure, noteValue, this, this.horPlaceForNextSymbol);
+        this.addSymbol(note);
+    }
+
+    addRest(restFigure) {
+        let rest = new Rest(restFigure, this, this.horPlaceForNextSymbol);
+        this.addSymbol(rest);
+    }
+
+    /* ajout du symbole à la liste et mise à jour de l'emplacement où placer le prochain */
+    addSymbol(symbol) {
+        this.symbols.push(symbol);
+        this.horPlaceForNextSymbol += symbol.width + symbolSpacing;
+        this.drawSymbol(symbol, ctx);
+    }
+
+    /* affichage d'un symbole */
+    drawSymbol(symbol, ctx) {
+        symbol.draw(ctx);
+    }
+
+    /* suppression d'un symbole */
+    deleteSymbol(i) {
+        /* mise à jour de l'emplacement du prochain symbole */
+        this.horPlaceForNextSymbol -= this.symbols[i].width + symbolSpacing;
+        /* suppression du symbole de la liste */
+        this.symbols.splice(i, 1);
+
+        /* mise à jour des positions des symboles suivants */
+        for (let j = i; j < this.symbols.length; j++) {
+            let previousSymbol = this.symbols[j-1];
+            let symbol = this.symbols[j];
+            if (j == 0) {
+                symbol.setHorPlaceInStaff(0);
+            }
+            else {
+                symbol.setHorPlaceInStaff(previousSymbol.horPlaceInStaff + previousSymbol.width + symbolSpacing);
+            }
+        }
+        /* on efface le canvas et on réaffiche la portée */
+        ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+        this.draw(ctx);
+    }
+}
+
+/* mode (ajout / suppression de notes) */
+class Mode {
+    constructor() {
+        this.value = "add_note";
+        this.noteFigure = wholeNote;
+    }
+
+    toDeleteNote() {
+        this.value = "delete_note";
+    }
+
+    toAddNote() {
+        this.value = "add_note";
+    }
+}
+
+const myStaff = new Staff(50, 110, 700);
 
 let wholeNote = new NoteFigure("whole_note");
 
+/* création d'une liste de hauteurs de notes que l'on pourra utiliser */
 const allNoteValues = [];
 let re4 = new NoteValue("D", 4, "natural", 293.66, 8);
 allNoteValues.push(re4);
@@ -163,16 +336,75 @@ allNoteValues.push(fa5);
 let sol5 = new NoteValue("G", 5, "natural", 783.99, -2);
 allNoteValues.push(sol5);
 
+const mode = new Mode();
+
+/* ajoute une note ou non en fonction de la position du clic */
+function addNoteOnClick(x, y) {
+    if (x > myStaff.xpos + myStaff.horPlaceForNextSymbol && x < myStaff.xpos + myStaff.horPlaceForNextSymbol + wholeNote.width) {
+        for (let i = 0; i < allNoteValues.length; i++) {
+            let yposInStaff = y - myStaff.ypos - staffLineSpacing/2;
+            if (( yposInStaff > (allNoteValues[i].vertPlaceInStaff - 1/2) * staffLineSpacing/2) && (yposInStaff < (allNoteValues[i].vertPlaceInStaff + 1/2) * staffLineSpacing/2)) {
+                myStaff.addNote(wholeNote, allNoteValues[i]);
+            }
+        }
+    }
+}
+
+/* supprime une note ou non en fonction de la position du clic */
+function deleteNoteOnClick(x, y) {
+    for (let i = 0; i < myStaff.symbols.length; i++) {
+        let symbol = myStaff.symbols[i];
+        /*if (symbol.classList.contains("Note")) {*/
+            if ((x > symbol.xpos) && (x < symbol.xpos + symbol.width) && (y > symbol.ypos) && (y < symbol.ypos + symbol.height)) {
+                myStaff.deleteSymbol(i);
+            /*}*/
+        }
+    }
+}
+
+/* effectue une action lors d'un clic */
 canvas.addEventListener("click", (event) => {
     const rect = canvas.getBoundingClientRect();
     const x = event.clientX - rect.left;
     const y = event.clientY - rect.top;
-    if (x > staff.xpos + staff.horPlaceForNextSymbol) {
-        for (let i = 0; i < allNoteValues.length; i++) {
-            let yposInStaff = y - staff.ypos - staffLineSpacing/2;
-            if (( yposInStaff > (allNoteValues[i].vertPlaceInStaff - 1/2) * staffLineSpacing/2) && (yposInStaff < (allNoteValues[i].vertPlaceInStaff + 1/2) * staffLineSpacing/2)) {
-                staff.addNote(wholeNote, allNoteValues[i]);
-            }
-        }
+    if (mode.value == "add_note") {
+        addNoteOnClick(x, y);
+    }
+    if (mode.value == "delete_note") {
+        deleteNoteOnClick(x, y);
     }
 })
+
+/* effectue une action lorsque l'on appuie sur une touche du clavier */
+window.addEventListener("keydown", (event) => {
+    if (event.key == "a") {
+        mode.toAddNote();
+    }
+    if (event.key == "d") {
+        mode.toDeleteNote();
+    }
+})
+
+/* avoir une note qui suit le déplacement de la souris de cette manière */
+/* fait laguer parce qu'il faut tout redessiner en permanence */
+/*
+let noteGrise = new Note(wholeNote, re4, staff, staff.horPlaceForNextSymbol);
+
+(function() {
+    document.onmousemove = handleMouseMove;
+    function handleMouseMove(event) {
+
+        let xpos = event.clientX;
+        let ypos = event.clientY;
+        if (xpos > staff.horPlaceForNextSymbol - 50 && xpos < staff.horPlaceForNextSymbol + 50) {
+            ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+            staff.draw(ctx);
+            noteGrise.xpos = xpos;
+            noteGrise.ypos = ypos;
+            noteGrise.draw(ctx);
+        }
+        
+
+    }
+})();
+*/
